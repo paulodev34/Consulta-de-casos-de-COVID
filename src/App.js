adb4616994a1274644717235941f52ca0404af04
Billy paulinho <paulo_neal@hotmail.com>
2022-09-11 19:05:45 -0300

    primeiro commit de consulta de casos covid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [estado, pais] = useState("1");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="select-estado">
+        <label>Estado </label>
+        <br />
+        <select name="estado" value={estado}>
+          <option value="">selecione</option>
+          <option value="state">sp</option>
+        </select>
+        <br />
+
+        <div className="select-pais">
+          <label>Pais </label>
+          <br />
+          <select name="pais" value={pais}>
+            <option value="">pais</option>
+            <option value="uf">Brasil</option>
+          </select>
+        </div>
+
+        <div className="formcontrol">
+          <label>Data de Nascimento</label>
+          <br />
+          <input type="date" id="data" data-rules="required" />
+          <br />
+        </div>
+      </div>
+      <button className="consulta" type="submit">
+        Consulta
+      </button>
     </div>
   );
-}
+};
 
 export default App;
